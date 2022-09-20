1b0aed2349d8b0c79616aa0296ef2526a0ce660e
lorena-21 <110685156+lorena-21@users.noreply.github.com>
2022-09-20 17:28:44 -0500

    trabajo de entrada y salida 4

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-    
-      private final static double PI = 3.14;
+
+      private final static double PII = 3.14;
       private static int numero1;
 
       public static void main(String[] args) {
           Scanner input = new Scanner(System.in);
           System.out.println("Digite su primer numero");
           numero1 = input.nextInt();
-          System.out.println("Resultado:  " + (numero1 * PI));
+          System.out.println("Resultado:  " + (numero1 * PII));
 
            }
     }
